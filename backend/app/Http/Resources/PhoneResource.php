01180b48dc050965f7f7decf9db493717eb17e67
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-08-04 13:03:17 +0300

    feat: ресурс для телефонов

---
+++ backend/app/Http/Resources/PhoneResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\OrganizationPhone;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin OrganizationPhone
+ */
+class PhoneResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'phone' => $this->phone
+        ];
+    }
+}
