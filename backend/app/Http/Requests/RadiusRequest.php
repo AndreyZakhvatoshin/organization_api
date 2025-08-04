04e565eba69b4c6d0d321c96a04e1b7c3ee8b3e9
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-08-04 13:03:29 +0300

    feat: валидация радиуса

---
+++ backend/app/Http/Requests/RadiusRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RadiusRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'lat' => 'required|numeric|between:-90,90',
+            'lng' => 'required|numeric|between:-180,180',
+            'radius' => 'required|numeric|min:0'
+        ];
+    }
+}
